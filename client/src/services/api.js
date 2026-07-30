1dd79f83ace74c451ebf99a1911b2578be27cc17
Rofhiwa” <rofhiwabold@gmail.com>
2026-07-30 03:44:36 +0200

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://airbnb-clone-backend-tq5h.onrender.com/api",
 });
 
 export default API;
